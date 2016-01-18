a8c9c6b826bbcf0ddc60b1c22f1b1d10f14e8fd0
Javier Collado <javier.collado@gmail.com>
2016-01-18 14:04:46 +0100

    feat(index): Promisify functions directly
    Functions are objecs as well, so they need to be promisifed, but without going through properties as
    in regular objects

--- index.js
+++ index.js
@@ -3,9 +3,14 @@
 const promisifyFn = require('promisify-function')
 
 function promisify (obj, fnNames) {
+  if (typeof obj === 'function') {
+    return promisifyFn(obj)
+  }
+
   fnNames.forEach(function (fnName) {
     obj[fnName] = promisifyFn(obj[fnName])
   })
+  return obj
 }
 
 module.exports = promisify
