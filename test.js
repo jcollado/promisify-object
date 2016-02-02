0ea9fc782f5541da9ac02b587a0951b0217739db
Javier Collado <javier.collado@gmail.com>
2016-02-02 21:14:13 +0100

    refactor(test): Update tests to use ava instead of mocha

--- test.js
+++ test.js
@@ -1,42 +1,42 @@
-/* global describe it beforeEach */
+import 'babel-register'
+
 import chai from 'chai'
 import requireInject from 'require-inject'
 import sinon from 'sinon'
+import test from 'ava'
 
 const expect = chai.expect
 
-describe('promisify-object', () => {
-  let stubs
+let stubs
 
-  beforeEach(() => {
-    stubs = {
-      'promisify-function': sinon.stub().returns('promisified')
-    }
+test.beforeEach(() => {
+  stubs = {
+    'promisify-function': sinon.stub().returns('promisified')
+  }
+})
+
+test('promisify-object: promisifies functions in object', () => {
+  const promisify = requireInject('./src/index', stubs).default
+
+  const obj = {
+    a: 'a function',
+    b: 'another function',
+    c: 'something else'
+  }
+
+  promisify(obj, ['a', 'b'])
+  expect(obj).to.deep.equal({
+    a: 'promisified',
+    b: 'promisified',
+    c: 'something else'
   })
+})
 
-  it('promisifies functions in object', () => {
-    const promisify = requireInject('./src/index', stubs).default
+test('promisify-object: promisifies functions', () => {
+  const promisify = requireInject('./src/index', stubs).default
 
-    const obj = {
-      a: 'a function',
-      b: 'another function',
-      c: 'something else'
-    }
+  const fn = () => {}
 
-    promisify(obj, ['a', 'b'])
-    expect(obj).to.deep.equal({
-      a: 'promisified',
-      b: 'promisified',
-      c: 'something else'
-    })
-  })
-
-  it('promisifies functions', () => {
-    const promisify = requireInject('./src/index', stubs).default
-
-    const fn = () => {}
-
-    const promisified = promisify(fn)
-    expect(promisified).to.equal('promisified')
-  })
+  const promisified = promisify(fn)
+  expect(promisified).to.equal('promisified')
 })
