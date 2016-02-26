071dc76ec70cd0dd0fb17c9cb462a1d9ef071f3a
Javier Collado <javier.collado@gmail.com>
2016-02-26 23:48:58 +0100

    docs(index): Add module jsdoc comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+/**
+ * Promisify functions in an object
+ * @module  promsify-object
+ */
 import promisifyFn from 'promisify-function'
 
+/**
+ * Promisify functions in an object
+ * @param {Object} obj - An object that contains some functions
+ * @param {Array} fnNames - Names of the functions to promisify
+ */
 export default function promisify (obj, fnNames) {
   if (typeof obj === 'function') {
     return promisifyFn(obj)
